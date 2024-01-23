f1c01918c49ce95eb3a5c69cd05685fe1c4de129
Nick Paduchowski <npaduchowski@hotmail.com>
2024-01-23 18:32:10 -0500

    Added functionality for the script to rotate through the different pages in a given category. Current categories are limited to pets and jobs.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 
 from flask import *
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -22,90 +23,191 @@ response = requests.get(page_1_url)
 
 #Use Beautiful Soup to parse HTML response
 
-soup = BeautifulSoup(response.text, "lxml")
+
 #print(soup)
+
+categories = {
+    'pets': {
+        'page1URL' : 'https://www.kijiji.ca/b-pets/canada/c112l0',
+        'nextPageTemplate': 'https://www.kijiji.ca/b-pets/canada/page-2/c112l0'
+    },
+    'jobs': {
+        'page1URL' : 'https://www.kijiji.ca/b-jobs/canada/c45l0'
+    }
+}
+def getNumOfPages(webpage):
+    pageNums = []
+    numberList = webpage.find_all("li", attrs={"data-testid": ["pagination-list-item"]})
+    for element in numberList:
+        element = element.find("a").text
+        num = element.split()
+        pageNums.append(int(num[-1]))
+    return pageNums[-1]
+
 
 # Get All The Ads
 
-ads = soup.find_all("h3", attrs={"data-testid": ["listing-title"]})
 
 # Remove marketing / third party ads
-filtered_ads = [x for x in ads if ("cas-channel" not in x["class"]) & ("third-party" not in x["class"])]
 
-ad_links = []
-for ad in filtered_ads:
-    #Parse the link from the ad
-    link = ad.find_all("a", {"data-testid": "listing-link"})
-    # Add the link to the list
-    for l in link:
-        ad_links.append(base_url + l["href"])
+def get_ad_info(pageNumber, category):
+    if (pageNumber==1):
+        ad_info = []
+        ad_links = []
+        page_url = categories[category]['page1URL']
+        print(page_url)
+        response = requests.get(page_url)
+        soup = BeautifulSoup(response.text, "lxml")
+        ads = soup.find_all("h3", attrs={"data-testid": ["listing-title"]})
+        filtered_ads = [x for x in ads if ("cas-channel" not in x["class"]) & ("third-party" not in x["class"])]
+        for ad in filtered_ads:
+            # Parse the link from the ad
+            link = ad.find_all("a", {"data-testid": "listing-link"})
+            # Add the link to the list
+            for l in link:
+                ad_links.append(base_url + l["href"])
+        for advert in (ad_links):
+                # Get Webpage Information & Parse With BeautifulSoup
+            print(advert)
+            response = requests.get(advert)
+            soup = BeautifulSoup(response.text, 'lxml')
 
-#@app.route('/', methods=['GET'])
-#def get_ad_info():
-ad_info = []
-for ad in filtered_ads:
-        # Parse the link from the ad
-    link = ad.find_all("a", {"data-testid": "listing-link"})
-        # Add the link to the list
-    for l in link:
-        ad_links.append(base_url + l["href"])
+                # Get Ad Title in Try/Catch Statement
+            try:
+                title = soup.find("h1").text
+            except AttributeError:
+                title = ""
 
-for advert in (ad_links):
-    # Get Webpage Information & Parse With BeautifulSoup
-    response = requests.get(advert)
-    soup = BeautifulSoup(response.text, 'lxml')
+                    # Get Ad Price
+            try:
+                 price = soup.find("span", attrs={"itemprop": "price"}).text
+            except AttributeError:
+                 price = ""
 
-        # Get Ad Title in Try/Catch Statement
-    try:
-        title = soup.find("h1").text
-    except AttributeError:
-        title = ""
+                # Get Date Posted
+            try:
+                  date_posted = soup.find("div", attrs={"itemprop": "datePosted"}).time['datetime']
+            except AttributeError:
+                date_posted = ""
 
-        # Get Ad Price
-    try:
-        price = soup.find("span", attrs={"itemprop": "price"}).text
-    except AttributeError:
-        price = ""
+                # Get Ad Description
+            try:
+                  description = soup.find("div", attrs={"itemprop": "description"}).text
+            except AttributeError:
+                 description = ""
 
-    # Get Date Posted
-    try:
-        date_posted = soup.find("div", attrs={"itemprop": "datePosted"})['content']
-    except AttributeError:
-        date_posted = ""
+                # Get Ad City
+            try:
+                adCity = soup.find("span", attrs={"itemprop": "address"}).text
+            except AttributeError:
+                adCity = ""
 
-    # Get Ad Description
-    try:
-        description = soup.find("div", attrs={"itemprop": "description"}).text
-    except AttributeError:
-        description = ""
+            try:
+                 mainImg_URL = soup.find("div", attrs={"class": "mainImage"}).picture.img['src']
+            except AttributeError:
+                mainImg_URL = ""
 
-    # Get Ad City
-    try:
-        adCity = soup.find("span", attrs={"itemprop": "address"}).text
-    except AttributeError:
-        adCity = ""
+            ad_info.append({
+                "category": category,
+                "title": title,
+                "price": price,
+                "description": description,
+                "date_posted": date_posted,
+                "address": adCity,
+                "url": advert,
+                "image_url": mainImg_URL
+             })
+        return ad_info
 
-    try:
-        mainImg_URL = soup.find("div", attrs={"class": "mainImage"}).picture.img['src']
-    except AttributeError:
-        mainImg_URL = ""
-    ad_info.append({
-        "title": title,
-        "price": price,
-        "description": description,
-        "date_posted": date_posted,
-        "address": adCity,
-        "url": advert,
-        "image_url": mainImg_URL
-    })
+    elif(pageNumber > 1):
+        ad_info = []
+        ad_links = []
+        page_url = 'https://www.kijiji.ca/b-' + category + '/canada/page-' + str(pageNumber) + '/c112l0'
+        print(page_url)
+        response = requests.get(page_url)
+        soup = BeautifulSoup(response.text, "lxml")
+        ads = soup.find_all("h3", attrs={"data-testid": ["listing-title"]})
+        filtered_ads = [x for x in ads if ("cas-channel" not in x["class"]) & ("third-party" not in x["class"])]
+        for ad in filtered_ads:
+                # Parse the link from the ad
+            link = ad.find_all("a", {"data-testid": "listing-link"})
+                # Add the link to the list
+            for l in link:
+                ad_links.append(base_url + l["href"])
+        for advert in (ad_links):
+            print(advert)
+            # Get Webpage Information & Parse With BeautifulSoup
+            response = requests.get(advert)
+            soup = BeautifulSoup(response.text, 'lxml')
+
+                # Get Ad Title in Try/Catch Statement
+            try:
+                title = soup.find("h1").text
+            except AttributeError:
+                title = ""
+
+                # Get Ad Price
+            try:
+                price = soup.find("span", attrs={"itemprop": "price"}).text
+            except AttributeError:
+                price = ""
+
+            # Get Date Posted
+            try:
+                date_posted = soup.find("div", attrs={"itemprop": "datePosted"}).time['datetime']
+            except AttributeError:
+                date_posted = ""
+                continue
+            except TypeError:
+                date_posted = ""
+                continue
+
+            # Get Ad Description
+            try:
+                description = soup.find("div", attrs={"itemprop": "description"}).text
+            except AttributeError:
+                description = ""
+
+            # Get Ad City
+            try:
+                adCity = soup.find("span", attrs={"itemprop": "address"}).text
+            except AttributeError:
+                adCity = ""
+
+            try:
+                mainImg_URL = soup.find("div", attrs={"class": "mainImage"}).picture.img['src']
+            except AttributeError:
+                mainImg_URL = ""
+
+            ad_info.append({
+                "category": category,
+                "title": title,
+                "price": price,
+                "description": description,
+                "date_posted": date_posted,
+                "address": adCity,
+                "url": advert,
+                "image_url": mainImg_URL
+            })
+
+        return ad_info
+
+def loadDatabase():
+    print("Loading the Database")
+    for obj in categories:
+        #print(categories[obj]['page1URL'])
+        response = requests.get(categories[obj]['page1URL'])
+        webpage =  BeautifulSoup(response.text, "lxml")
+        numOfPages = getNumOfPages(webpage)
+        for x in range(1, numOfPages):
+            print(x)
+            result = get_ad_info(x, obj)
+            print(result)
 
 
-print(ad_info)
-
-
+loadDatabase()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-#    app.run(port=3333)
-   print("Hello")
+    print("Script Running")
