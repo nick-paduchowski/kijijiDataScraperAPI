a9817d1215e02e5504651e28155fbd187c841456
Nick Paduchowski <npaduchowski@hotmail.com>
2024-02-02 16:54:46 -0500

    Added some basic error handling and input validation

--- src/controller.js
+++ src/controller.js
@@ -1,55 +1,64 @@
 const pool = require("../db");
 const queries = require("./queries");
 
-const validQueryParams = [
-  'category',
-  'city',
-  'province',
-  'job-type'
-]
+const validQueryParams = ["category", "city", "province", "job-type"];
 
 const displayWelcomeMessage = (req, res) => {
   try {
     res.status(200).json({
-        "Hello!": "Welcome to the Kijiji Ads Data API.",
-        "Current Jobs Data Available": "All Job Data is available to fetch.",
-      });
-    console.log("Request Received")
+      "Hello!": "Welcome to the Kijiji Ads Data API.",
+      "Current Jobs Data Available": "All Job Data is available to fetch.",
+    });
+    console.log("Request Received");
   } catch (error) {
     res.status(500).json({
-        "Error": "There was an error with your request. Try again later."
-    })
+      Error: "There was an error with your request. Try again later.",
+    });
   }
-  
 };
 
 const getJobsByCategory = (req, res) => {
-    const category = req.query.category;
-    console.log(category);
-    pool.query(queries.getJobsByCategory, [category], (error, results) => {
-        if (error) throw error;
-        res.status(200).json(results.rows);
-    });
-}
-
-const getAllJobs = (req,res) => {
-    pool.query(queries.getAllJobs, (error, results) => {
-        if (error) {
-          console.log("An error occurred")
-          throw error
-        }
-        res.status(200).json(results.rows);
-    })
-}
-
-const getJobs= (req, res) => {
-  const queryInfo = req.query;
-  console.log(queryInfo)
-  pool.query(queries.buildQuery(queryInfo), (error, results) => {
+  const category = req.query.category;
+  console.log(category);
+  pool.query(queries.getJobsByCategory, [category], (error, results) => {
     if (error) throw error;
     res.status(200).json(results.rows);
-  })
-}
+  });
+};
+
+const getAllJobs = (req, res) => {
+  pool.query(queries.getAllJobs, (error, results) => {
+    if (error) {
+      console.log("An error occurred");
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+
+const getJobs = (req, res) => {
+  const queryInfo = req.query;
+  const queryDB = true;
+  for (let query in queryInfo) {
+    if (!validQueryParams.includes(query)) {
+      res.status(400).json({
+        "Bad Request": "The request you sent includes non-valid parameters.",
+        "Non-Valid Parameter": `${query}`,
+      });
+      queryDB = false;
+      break;
+    }
+  }
+  if (queryDB === true){
+    pool.query(queries.buildQuery(queryInfo), (error, results) => {
+      if (error){
+        throw error;
+      } else {
+        res.status(200).json(results.rows);
+      }
+    });
+  }
+};
 
 module.exports = {
   displayWelcomeMessage,
--- src/queries.js
+++ src/queries.js
@@ -1,5 +1,3 @@
-const { param } = require("./routes");
-
 const getAllJobs = "SELECT * FROM job_data";
 const getJobsByCategory = "SELECT * FROM job_data WHERE category = $1"
 
